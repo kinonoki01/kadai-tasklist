a9a971846748df13bb25bcbbdf1608b546fbdd38
daichi.maruyama <kinonoki01@gmail.com>
2022-01-14 03:13:31 +0000

    add tasks CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,55 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @tasks = Task.all
   end
   
   def show
   end
   
   def new
+    @task = Task.new
   end
   
   def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'タスクを作成しました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクの作成に失敗しました'
+      render :new
+    end
   end
   
   def edit
   end
   
   def update
+    if @task.update(task_params)
+      flash[:success] = 'タスクを更新しました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクの更新に失敗しました'
+      render :edit
+    end
   end
   
   def destroy
+    @task.destroy
+    flash[:success] = 'タスクを削除しました'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
